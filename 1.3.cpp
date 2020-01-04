99b15e142ec8d2e1f28bc50624acba2374e801ba
HaroldCC <2106562095@qq.com>
2020-01-04 15:37:21 +0800

    first commit

---
+++ 1.3.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+    > File Name       : 1.3.cpp
+    > Author          : Harold
+    > Mail            : 2106562095@qq.com
+    > Github          : www.github.com/Haroldcc
+    > Created Time    : 2020-01-01 19:08:52
+ ************************************************************************/
+#include<iostream>
+
+using namespace std;
+
+int main(int argc,char *argv[])
+{
+	cout << "Hello, Word。";
+     
+    return 0;
+}
+
